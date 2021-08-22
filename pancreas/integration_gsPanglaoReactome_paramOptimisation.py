85cf3586082e0880c7a88c3ae302cc16fd0b3e32
ICB Karin Hrovatin <karin.hrovatin@icb-lisa.scidom.de>
2021-08-22 13:02:39 +0200

    different ref and query, more params

--- pancreas/integration_gsPanglaoReactome_paramOptimisation.py
+++ pancreas/integration_gsPanglaoReactome_paramOptimisation.py
@@ -5,23 +5,35 @@ import numpy as np
 import argparse
 from pathlib import Path
 from matplotlib import rcParams
+import pickle
+import torch
+import sys
 
 import scarches as sca
 from scarches.dataset.trvae.data_handling import remove_sparsity
 # -
 
 path_data='/storage/groups/ml01/workspace/karin.hrovatin/data/pancreas/scRNA/qtr/'
-path_save_base=path_data+'integrated/gsCellType_query/'
+# This was set to ending in querySpikeinDrug/ when we used all studies and different query
+path_save_base=path_data+'integrated/gsCellType_query/querySTZ/'
 path_gmt='/storage/groups/ml01/code/karin.hrovatin//qtr_intercode_reproducibility-/metadata/'
 
+# +
 parser = argparse.ArgumentParser()
 def intstr_to_bool(x):
     return bool(int(x))
 def hidden_layers_parser(hls):
     return [int(i) for i in hls.split(',')]
+def parse_alpha(alpha):
+    if ',' in alpha:
+        alpha=[float(a) for a in alpha.split(',')]
+    else:
+        alpha=float(alpha)
+    return alpha
+        
 parser.add_argument('-d','--dir',type=str,
                    help='Subdir added to base path for saving')
-parser.add_argument('-a','--alpha', type=float,
+parser.add_argument('-a','--alpha', type=parse_alpha,
                     help='Training alpha') 
 parser.add_argument('-akl','--alpha_kl',  type=float,
                     help='Training alpka_kl')
@@ -48,13 +60,26 @@ parser.add_argument('-ne','--n_epochs', default=400, type=int,
                     help='Max N epochs for training')
 parser.add_argument('-s','--save', default=False, type=intstr_to_bool,
                     help='Save model and latent data')
+parser.add_argument('-dll','--decoder_last_layer',type=str,default='softmax',
+                   help='Last layer type of decoder')
+parser.add_argument('-ule','--use_l_encoder',type=intstr_to_bool,default=False,
+                   help='Use library size encoder')
+parser.add_argument('-mg','--max_genes',type=int,default=None,
+                   help='Largest gene set size to use. Default is None (no limit).'+\
+                    'Should be int otherwise')
 
 
-# Just for testing without command line
+# +
+# Just for testing without command line or saving data
+
+# Only for saving data for other integaryion methods
+saving_expression=False
+
+# testing
 if False:
     args= parser.parse_args(args=[
         '-d','test/',
-        '-a','0.1',
+        '-a','0.1,0.9',
         '-akl','10',
         '-ra','0',
         '-uh','0',
@@ -62,23 +87,41 @@ if False:
         '-sd','1',
         '-nh','5000',
         '-ne','10',
-        '-s','1'
+        '-s','1',
+        '-ule','1',
+        '-mg','200',
     ])
+# Saving
+elif saving_expression:
+    args= parser.parse_args(args=[
+        '-d','model/',
+        '-ra','0',
+        '-uh','0',
+        '-mg','200'
+    ])
+
 # Read command line args
 else:
+    print(sys.argv)
     args = parser.parse_args()
+
+# -
 
 path_save=path_save_base+args.dir
 
-args_name='alpha_'+str(args.alpha)+'-alpha_kl_'+str(args.alpha_kl)+\
-    '-remove_ambient_'+str(int(args.remove_ambient))+\
-    '-use_hvg_'+str(int(args.use_hvg))+'-batch_'+args.batch+\
-    '-subset_data_'+str(args.subset_data)+'-data_proportion_'+str(args.data_proportion)+\
-    '-learning_rate_'+str(args.learning_rate)+\
-    '-hidden_layer_sizes_'+'.'.join([str(s) for s in args.hidden_layer_sizes])+\
-    '-early_stopping_'+str(int(args.early_stopping))+\
-    '-n_hvg_'+str(args.n_hvg)+\
-    '-n_epochs_'+str(args.n_epochs)
+args_name='a_'+str(args.alpha).replace(' ','').replace('[','p').replace(']','').replace(',','r')+\
+    '-akl_'+str(args.alpha_kl)+\
+    '-ra_'+str(int(args.remove_ambient))+\
+    '-uh_'+str(int(args.use_hvg))+'-b_'+str(args.batch)+\
+    '-sd_'+str(args.subset_data)+'-dp_'+str(args.data_proportion)+\
+    '-lr_'+str(args.learning_rate)+\
+    '-hls_'+'.'.join([str(s) for s in args.hidden_layer_sizes])+\
+    '-es_'+str(int(args.early_stopping))+\
+    '-nh_'+str(args.n_hvg)+\
+    '-ne_'+str(args.n_epochs)+\
+    '-dll_'+str(args.decoder_last_layer)+\
+    '-ule_'+str(args.use_l_encoder)+\
+    '-mg_'+str(args.max_genes)
 print('Args:',args_name)
 
 # ## Prepare ref and query
@@ -87,9 +130,15 @@ print('Args:',args_name)
 adata=sc.read(path_data+'adata_annotated.h5ad')
 print('Whole shape (non-raw):',adata.shape)
 
+# Omit study from ref and query
+study_omit=None # remove this study 
+if study_omit is not None:
+    adata=adata[adata.obs.study!=study_omit,:]
+    print('Whole filtered shape (non-raw):',adata.shape)
+
 # Query info
 ct_query='immune' # Cell types containing this word are removed from reference (but not query)
-study_query='spikein_drug' # remove this study from reference and use as query
+study_query='STZ' # remove this study from reference and use as query
 adata.obs['ref_query']=['ref' if study!=study_query else 'query'
                            for study in adata.obs.study]
 
@@ -126,11 +175,11 @@ print('Ref filtered shape:',adata_r.shape)
 
 # +
 # Add gene set anno to ref
-# Omit gene sets not having at least 5 genes
+# Omit gene sets not having at least 5 genes or too many genes
 sca.add_annotations(adata_r, 
                     [path_gmt+'PanglaoDB_markers_27_Mar_2020_mouseEID.gmt',
                     path_gmt+'c2.cp.reactome.v4.0_mouseEID.gmt'], 
-                    min_genes=5, clean=False)
+                    min_genes=5, max_genes=args.max_genes, clean=False)
 print('N used gene sets:',adata_r.varm['I'].shape[1])
 
 # Subset data to only genes in added gene sets
@@ -145,7 +194,7 @@ adata_q.uns['terms']=adata_r.uns['terms'].copy()
 print('Query shape:',adata_q.shape)
 
 
-# ## Training
+# Prepare data objects used in training
 
 # Use whole datasets or only subset for testing out the scripts
 def subset_data(adata,proportion=args.data_proportion):
@@ -164,6 +213,43 @@ else:
 adata_sub=adata_sub.raw.to_adata()[:,adata_r_sub.var_names]
 print('Ref:',adata_r_sub.shape,'Query:',adata_q_sub.shape,'All:',adata_sub.shape)
 
+# Save data for other integration methods
+# Subset only to query and ref, not all cells
+if saving_expression:
+    adata_integration=adata_sub[adata_r_sub.obs_names.to_list()+\
+                                adata_q_sub.obs_names.to_list(),:].copy()
+    # Query-ref info
+    adata_integration.obs.loc[adata_r_sub.obs_names,'ref_query']='ref'
+    adata_integration.obs.loc[adata_q_sub.obs_names,'ref_query']='query'
+    # Save
+    print('Integration data shape:',adata_integration.shape)
+    adata_integration.write(path_save_base+'model/adata_integration_RefQueryTraining.h5ad')
+    print(path_save_base+'model/adata_integration_RefQueryTraining.h5ad')
+    del adata_integration
+    # Integration data shape: (82077, 6571)
+    # /storage/groups/ml01/workspace/karin.hrovatin/data/pancreas/scRNA/qtr/integrated/gsCellType_query/querySTZ_rmNodElimination/model/adata_integration_RefQueryTraining.h5ad
+    # Integration data shape: (133213, 6849)
+    # /storage/groups/ml01/workspace/karin.hrovatin/data/pancreas/scRNA/qtr/integrated/gsCellType_query/querySTZ/model/adata_integration_RefQueryTraining.h5ad
+    
+    # Save terms info
+    pickle.dump(np.array(adata_r.uns['terms']),open(
+        path_save_base+'model/terms.pkl',
+        'wb'))
+
+# ## Training
+
+# Set alpah and omegas
+if isinstance(args.alpha,float):
+    alpha=args.alpha
+    omega=None
+else:
+    omega = torch.full((len(adata_r.uns['terms']),1),fill_value=float(1)).view(-1)
+    is_panglao=[t.startswith('PANGLAO') for t in adata_r.uns['terms']]
+    is_reactome=[t.startswith('REACTOME') for t in adata_r.uns['terms']]
+    omega[is_panglao] = args.alpha[0]
+    omega[is_reactome] = args.alpha[1]
+    alpha=float(1)
+
 # ### Create TRVAE model and train it on reference dataset
 
 model = sca.models.TRVAE(
@@ -175,7 +261,9 @@ model = sca.models.TRVAE(
     #beta=0.1,
     mask=adata_r.varm['I'].T,
     use_decoder_relu=False,
-    mmd_instead_kl=False
+    mmd_instead_kl=False,
+    decoder_last_layer=args.decoder_last_layer,
+    use_l_encoder=args.use_l_encoder,
 )
 
 early_stopping_kwargs = {
@@ -190,7 +278,8 @@ model.train(
     lr=args.learning_rate,
     n_epochs=args.n_epochs,
     alpha_epoch_anneal=None, 
-    alpha=args.alpha, # Higehr more integration
+    alpha=alpha, # Higehr more integration
+    omega=omega,
     alpha_kl=args.alpha_kl, # Higehr more integration
     early_stopping_kwargs=early_stopping_kwargs,
     use_early_stopping=args.early_stopping, 
